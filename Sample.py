bf6659579526008829ba289931ebbd4c917e710d
8838satheesh <sk73050091@gmail.com>
2025-05-06 13:15:18 +0530

    Create Sample.py

---
+++ Sample.py
@@ -0,0 +1,13 @@
+import streamlit as st
+st.title("satheesh")
+st.sidebar.title("Give us your valid Info")
+st.sidebar.text_input("Enter your Email ")
+if(st.sidebar.button("Register")):
+    st.sidebar.success("Your email verified")
+st.text_input("Enter your name ")
+st.date_input("Enter your DOB ")
+st.text_input("Enter your Mobile Number")
+st.radio("Select your Gender",["Male","Female","Others"])
+if(st.button("Submit")):
+    st.write("Thank you for yor registration")
+    st.success("Your Completed successfully")
